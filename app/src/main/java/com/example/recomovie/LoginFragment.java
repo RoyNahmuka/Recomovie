141d47b596c5b313be6c698f47d5bffc34b74e8b
shovalnissim <shovalniss@gmail.com>
2022-03-05 17:20:15 +0200

    redirect the buttons

--- app/src/main/java/com/example/recomovie/LoginFragment.java
+++ app/src/main/java/com/example/recomovie/LoginFragment.java
@@ -3,18 +3,33 @@ package com.example.recomovie;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 public class LoginFragment extends Fragment {
-
+    Button goToRegister;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view= inflater.inflate(R.layout.fragment_login, container, false);
+        goToRegister=view.findViewById(R.id.go_to_register);
+        goToRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RegisterFragment RegisterFragment = new RegisterFragment();
+                FragmentTransaction tran = getParentFragmentManager().beginTransaction();
+                tran.add(R.id.base_frag_container, RegisterFragment);
+                tran.addToBackStack("");
+                tran.commit();
+            }
+        });
+        return  view;
+
     }
 }
--- app/src/main/java/com/example/recomovie/ReviewListRvFragment.java
+++ app/src/main/java/com/example/recomovie/ReviewListRvFragment.java
@@ -24,12 +24,11 @@ import java.util.List;
 public class ReviewListRvFragment extends Fragment {
     List<Review> reviewList = new LinkedList<>();
     Button addReviewBtn;
+    Button loginBtn;
 
     interface OnItemClickListener {
         void onItemCLick(int position);
     }
-
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -67,8 +66,20 @@ public class ReviewListRvFragment extends Fragment {
                 tran.commit();
             }
         });
+        loginBtn = view.findViewById(R.id.login_button);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LoginFragment LoginFragment= new LoginFragment();
+                FragmentTransaction tran = getParentFragmentManager().beginTransaction();
+                tran.add(R.id.base_frag_container, LoginFragment);
+                tran.addToBackStack("");
+                tran.commit();
+            }
+        });
         return view;
     }
+
 
     class ReviewListViewHolder extends RecyclerView.ViewHolder{
         TextView movieName;
@@ -121,6 +132,64 @@ public class ReviewListRvFragment extends Fragment {
         @Override
         public int getItemCount() {
             return reviewList.size();
+        }
+    }
+
+    /**
+     * A simple {@link Fragment} subclass.
+     * Use the {@link ReviewPageFragment#newInstance} factory method to
+     * create an instance of this fragment.
+     */
+    public static class ReviewPageFragment extends Fragment {
+
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_REVIEW_ID = "ARG_REVIEW_ID";
+        private String reviewId;
+
+        public ReviewPageFragment() {
+
+        }
+
+        public static ReviewPageFragment newInstance(String reviewId) {
+            ReviewPageFragment fragment = new ReviewPageFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_REVIEW_ID, reviewId);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+                reviewId = getArguments().getString(ARG_REVIEW_ID);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_review_page, container, false);
+            Review clickedReview = Model.instance.getReviewByIndex(Integer.valueOf(reviewId));
+
+            TextView movieName = view.findViewById(R.id.review_info_input_movie_name);
+            TextView description = view.findViewById(R.id.review_info_input_description);
+            TextView category = view.findViewById(R.id.review_info_input_category);
+    //        TextView actors = findViewById(R.id.review_info_input_actors);
+            TextView year = view.findViewById(R.id.review_info_input_year);
+            TextView country = view.findViewById(R.id.review_info_input_country);
+            TextView rate = view.findViewById(R.id.review_info_input_stars);
+
+            movieName.setText(clickedReview.getMovieName());
+            description.setText(clickedReview.getDescription());
+            category.setText(clickedReview.getCategory());
+    //        actors.setText(clickedReview.getActors());
+            year.setText(clickedReview.getYear());
+            country.setText(clickedReview.getCountry());
+    //        rate.setText(clickedReview.getStars());
+            return view;
         }
     }
 }
